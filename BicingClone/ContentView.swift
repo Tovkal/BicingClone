e5b145ecd0580465edff0913205df9e5a9782502
Andrés Pizá <andres.piza@scrm.eu>
2019-11-09 12:22:14 +0100

    Create MapView

--- BicingClone/ContentView.swift
+++ BicingClone/ContentView.swift
@@ -1,21 +1,33 @@
-//
-//  ContentView.swift
-//  BicingClone
-//
-//  Created by Andrés Pizá Bückmann on 09/11/2019.
-//  Copyright © 2019 Andrés Pizá Bückmann. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, World!")
+        MapView()
+            .edgesIgnoringSafeArea([.top, .bottom])
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    typealias UIViewType = MKMapView
+
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        let mapView = MKMapView()
+        let center = CLLocationCoordinate2D(latitude: 41.4024, longitude: 2.1952)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: false)
+        return mapView
+    }
+
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
     }
 }
