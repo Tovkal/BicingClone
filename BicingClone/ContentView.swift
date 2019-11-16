7d5caea4d543bf061a1cdd36cb8ffe0a266766db
Andrés Pizá <andres.piza@scrm.eu>
2019-11-16 11:48:34 +0100

    Display stations in map

--- BicingClone/ContentView.swift
+++ BicingClone/ContentView.swift
@@ -1,9 +1,16 @@
 import SwiftUI
+import Combine
+
+struct Station {
+    let latitude: Double
+    let longitude: Double
+}
 
 struct ContentView: View {
 
     var body: some View {
         MapView()
+            .environmentObject(MapViewViewModel())
             .edgesIgnoringSafeArea([.top, .bottom])
     }
 }
@@ -19,6 +26,12 @@ import MapKit
 struct MapView: UIViewRepresentable {
     typealias UIViewType = MKMapView
 
+    @EnvironmentObject var viewModel: MapViewViewModel
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
     func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
         let mapView = MKMapView()
         let center = CLLocationCoordinate2D(latitude: 41.4024, longitude: 2.1952)
@@ -29,5 +42,27 @@ struct MapView: UIViewRepresentable {
     }
 
     func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        for station in viewModel.stations {
+            let annotation = MKPointAnnotation()
+            let coordinate = CLLocationCoordinate2D(latitude: station.latitude,
+                                                    longitude: station.longitude)
+            annotation.coordinate = coordinate
+            uiView.addAnnotation(annotation)
+        }
     }
+
+    class Coordinator: NSObject {
+        private let mapView: MapView
+
+        init(_ mapView: MapView) {
+            self.mapView = mapView
+        }
+    }
+}
+
+class MapViewViewModel: ObservableObject {
+    @Published var stations: [Station] = [
+        .init(latitude: 41.4048, longitude: 2.1939),
+        .init(latitude: 41.4024, longitude: 2.1929)
+    ]
 }
